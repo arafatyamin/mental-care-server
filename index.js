842c8385879c063f88c128fc5b0864e0ca6626de
arafatyamin <mdh631987@gmail.com>
2022-11-08 02:04:10 +0600

    connect mongo db

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('doctor server is running');
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.o0lhbrs.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollections = client.db('doctorsPortal').collection('services')
+
+        app.get('/services', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollections.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
+
+app.listen(port, () => {
+    console.log(`doctor portal running on:: ${port}`);
+})
