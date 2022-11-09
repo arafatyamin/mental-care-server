ba1456f2fe1e265a591082fe68d3e2eabd8fb191
arafatyamin <mdh631987@gmail.com>
2022-11-10 03:31:32 +0600

    get services with id

--- index.js
+++ index.js
@@ -34,7 +34,12 @@ async function run(){
             res.send(services)
         });
 
-        
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollections.findOne(query);
+            res.send(service);
+        });
         
         
 
